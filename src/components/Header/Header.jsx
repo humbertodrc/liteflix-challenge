087a5e1a2a58fb2644ec02789d2f684d92662679
danieldamian09 <humbertorivero9@gmail.com>
2022-02-24 21:12:09 -0300

    Add elements to the header, styles and responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import styles from "../../../styles/Header.module.css";
+import addMovie from "../../assets/img/agregar-película.png";
+import more from "../../assets/img/plus.png";
+import icono from "../../assets/img/menu.png";
+import notifications from "../../assets/img/notificacion.png";
+import photo from "../../assets/img/perfil.png";
 
 const Header = () => {
-  return (
-    <div>Desde Header</div>
-  )
-}
+	return (
+		<header className={styles.header}>
+			<div className={styles.header__actions}>
+				<img
+					className={styles.header__add}
+					src={addMovie}
+					alt="agregar pelicula"
+				/>
+				<div className={styles.header__separator}>
+          <a className={styles.header__more}>	<img className={styles.header__cross} src={more} alt="icono agregar pelicula" />agregar película</a>
+				</div>
+					<p className={styles.header__logo}>
+						LITE<span className={styles.header__modifier}>FLIX</span>
+					</p>
+			</div>
+			<div className={styles.header__menu}>
+				<img className={styles.header__icono} src={icono} alt="icono menu" />
+				<img
+					className={styles.header__notifications}
+					src={notifications}
+					alt="notificaciones"
+				/>
+				<img className={styles.header__avatar} src={photo} alt="foto perfil" />
+			</div>
+		</header>
+	);
+};
 
-export default Header
+export default Header;
