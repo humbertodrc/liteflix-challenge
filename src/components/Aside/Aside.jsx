d78daed7278af29507f702441e6bf83525cc0226
danieldamian09 <humbertorivero9@gmail.com>
2022-02-25 15:51:29 -0300

    add style component Aside with styled-component

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,9 +1,25 @@
-import styles from "../../../styles/Aside.module.css"
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+	width: 100%;
+	height: 800px;
+	background: linear-gradient(to top, rgb(0 0 0 / 99%), rgb(36 36 36 / 5%));
+	color: #fff;
+	@media (min-width: 768px) {
+		background: inherit;
+	}
+`;
 
 function Aside() {
-  return (
-    <div className={styles.aside}>Aside</div>
-  )
+	return (
+		<Container>
+			<label htmlFor="">Ver:</label>
+			<select name="" id="">
+				<option value="">Populares</option>
+				<option value="">Mis Peliculas</option>
+			</select>
+		</Container>
+	);
 }
 
-export default Aside
+export default Aside;
