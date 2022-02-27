a121c56b3799d2a8402c85988aef591a518b6095
danieldamian09 <humbertorivero9@gmail.com>
2022-02-27 15:25:45 -0300

    Querying the API from the Home component to display the featured movie and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import "./App.css";
 import Home from "./components/Home/Home";
 import Modal from "./components/Modal/Modal";
@@ -6,32 +6,10 @@ import Modal from "./components/Modal/Modal";
 function App() {
 	// Bandera pata mostar Modal
 	const [modal, setModal] = useState(false);
-	const [nowPlaying, setNowPlaying] = useState([])
-
-
-	const getAPINowPlaying = async() => {
-		const urlNowPlaying = `https://api.themoviedb.org/3/movie/now_playing?api_key=6f26fd536dd6192ec8a57e94141f8b20`
-		try {
-			const respuesta = await fetch(urlNowPlaying)
-			const {results} = await respuesta.json();
-			const shuffledArray = results.sort((a, b) => 0.5 - Math.random()).slice(0,1);
-			setNowPlaying(shuffledArray[0])
-		} catch (error) {	
-			return error
-		}
-	}
-
-	useEffect(() => {
-		getAPINowPlaying()
-	},[])
-
-	// Traer Datos de la API
-	const urlImageMovie = `https://image.tmdb.org/t/p/original/${nowPlaying?.poster_path}`
-	const titleMovie = `${nowPlaying?.original_title}`
-
+	
 	return (
 		<>
-			<Home setModal={setModal} urlImageMovie={urlImageMovie} titleMovie={titleMovie} />
+			<Home setModal={setModal}  />
 			{modal ? <Modal /> : null}
 		</>
 	);
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,56 @@
+import React, {useEffect, useState} from "react";
 import Header from "../Header/Header";
 import styles from "../../../styles/Home.module.css";
 import Title from "../Title/Title";
 import Aside from "../Aside/Aside";
 
-function Home({setModal, urlImageMovie, titleMovie}) {
+function Home({setModal}) {
+	const [nowPlaying, setNowPlaying] = useState([]);
+	const [load, setLoad] = useState(false);
 
-	const sectionStyle = {
-		backgroundImage: `linear-gradient( to bottom, rgb(11 11 11 / 40%), rgb(13 13 13 / 1%) ), url(${urlImageMovie})`
+	const getAPINowPlaying = async () => {
+		const urlNowPlaying = `https://api.themoviedb.org/3/movie/now_playing?api_key=6f26fd536dd6192ec8a57e94141f8b20`;
+		try {
+			const response = await fetch(urlNowPlaying);
+			const {results} = await response.json();
+			const shuffledArray = results
+				.sort((a, b) => 0.5 - Math.random())
+				.slice(0, 1);
+			console.log(shuffledArray)
+			setNowPlaying(shuffledArray[0]);
+		} catch (error) {
+			return error;
+		}
+	};
+
+	useEffect(() => {
+		setLoad(true);
+		getAPINowPlaying();
+		setLoad(false);
+	}, []);
+
+	// Traer Datos de la API
+	const urlImageMovie = `https://image.tmdb.org/t/p/original/${nowPlaying?.poster_path}`;
+	const titleMovie = `${nowPlaying?.original_title}`;
+
+	const sectionStyleMovie = {
+		backgroundImage: `linear-gradient( to bottom, rgb(11 11 11 / 40%), rgb(13 13 13 / 1%) ), url(${urlImageMovie})`,
 	};
 
 	return (
-		<div className={styles.container} style={sectionStyle}>
-			<Header setModal={setModal} />
-			<main className={styles.container__main}>
-				<Title titleMovie={titleMovie} />
-				<Aside />
-			</main>
-		</div>
+		<>
+			{load ? (
+				<p>Cargando...</p>
+			) : (
+				<div className={styles.container} style={sectionStyleMovie}>
+					<Header setModal={setModal} />
+					<main className={styles.container__main}>
+						<Title titleMovie={titleMovie} />
+						<Aside />
+					</main>
+				</div>
+			)}
+		</>
 	);
 }
 
