65fe343bb20f1efd37f7037bb661d46f8cc05055
danieldamian09 <humbertorivero9@gmail.com>
2022-02-27 20:23:08 -0300

    style title

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,8 +1,31 @@
 import styles from "../../../styles/Title.module.css"
+import styled from "@emotion/styled"
+
+const Heading = styled.h1`
+  font-size: 36px;
+  text-align: center;
+  margin: 10px auto;
+  letter-spacing: 12px;
+  color: #64EEBC;
+  text-transform: uppercase;
+  font-weight: bold;
+`
+
+const PreTitle = styled.p`
+  font-size: 20px;
+  line-height: 22px;
+  margin: 0;
+  letter-spacing: 4px;
+  color: #fff;
+  text-align: center;
+`
 
 function Title({titleMovie}) {
   return (
-    <div className={styles.title}>{titleMovie}</div>
+    <div className={styles.title}>
+      <PreTitle>ORIGINAL DE LITEFLIX</PreTitle>
+      <Heading>{titleMovie}</Heading>
+    </div>
   )
 }
 
