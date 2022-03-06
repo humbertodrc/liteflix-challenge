0308c1b6db294b4904255d4b3e25abe932414ffc
danieldamian09 <humbertorivero9@gmail.com>
2022-03-06 16:50:23 -0300

    migrate styles from module css to styled component Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,23 @@
-import React from "react";
-import styles from "../../../styles/Logo.module.css"
+import styled from "@emotion/styled";
+
+const LogoLite = styled.p`
+	font-size: 28px;
+	line-height: 28px;
+	letter-spacing: 4px;
+	font-weight: 700;
+	color: #64eebc;
+`
+
+const LogoModifier = styled.span`
+	font-weight: 400;
+`
 
 const Logo = () => {
 	return (
 		<div>
-			<p className={styles.header__logo}>
-				LITE<span className={styles.header__modifier}>FLIX</span>
-			</p>
+			<LogoLite>
+				LITE<LogoModifier >FLIX</LogoModifier>
+			</LogoLite>
 		</div>
 	);
 };
